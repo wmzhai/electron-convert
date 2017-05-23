e3fd6f7b056d458b70c5f03b1942837d697cd554
Weiming Zhai <wmzhai@gmail.com>
2017-05-23 18:27:12 +0800

    fetch metadata in promise

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const ffmpeg = require('fluent-ffmpeg')
 const { app, BrowserWindow, ipcMain } = electron
 
 let mainWindow
@@ -13,5 +14,11 @@ app.on('ready', () => {
 })
 
 ipcMain.on('videos:added', (event, videos) => {
-  console.log(videos)
+  const promise = new Promise((resolve, reject) => {
+    ffmpeg.ffprobe(videos[0].path, (err,metadata) => {
+      resolve(metadata)
+    })
+  })
+
+  promise.then((metadata) => { console.log(metadata)})
 })
