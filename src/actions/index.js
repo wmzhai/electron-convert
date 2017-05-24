096d90b2b2e87258f7a5ca8bd876e62275257d9b
Weiming Zhai <wmzhai@gmail.com>
2017-05-24 15:36:50 +0800

    progress

--- src/actions/index.js
+++ src/actions/index.js
@@ -17,8 +17,13 @@ export const addVideos = videos => dispatch => {
 // conversion.
 export const convertVideos = videos => dispatch => {
   ipcRenderer.send('conversion:start', videos)
+
   ipcRenderer.on('conversion:end', (event, {video, outputPath}) => {
     dispatch({ type: VIDEO_COMPLETE, payload: { ...video, outputPath }})
+  })
+
+  ipcRenderer.on('conversion:progress', (event, {video, timemark}) => {
+    dispatch({ type: VIDEO_PROGRESS, payload: { ...video, timemark }})
   })
 };
 
